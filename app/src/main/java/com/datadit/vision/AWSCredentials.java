7e5f4ebc35f2c071d048a314f921f3488abd2eab
Dhairya Kumar <36099337+Dhairya10@users.noreply.github.com>
2020-04-14 12:44:41 +0530

    Update AWSCredentials.java

--- app/src/main/java/com/datadit/vision/AWSCredentials.java
+++ app/src/main/java/com/datadit/vision/AWSCredentials.java
@@ -4,7 +4,7 @@ import com.amazonaws.regions.Regions;
 // Class for storing all the AWS Credentials
 public class AWSCredentials {
     // IAM Authorisation
-    private static final String COGNITO_POOL_ID =   "us-east-1:b82cb567-a0ae-4c4c-9ce1-eb4d87aaab02"; // Identity pool ID
+    private static final String COGNITO_POOL_ID =   "Enter your Your Cognito Pool ID here"; // Identity pool ID
     private static final Regions COGNITO_REGION = Regions.US_EAST_1;
 
 
